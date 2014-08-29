7e73c0900a6cb47f3afd1f4c8ad75510cba248d5
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-08-29 11:37:31 +0200

    exercise 2 done

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -15,88 +15,105 @@ import java.util.Map;
 /**
  * @author DHDC
  */
-public class FirstHtttpServer {
-  static int port = 8080;
-  static String ip = "127.0.0.1";
-  public static void main(String[] args) throws Exception {
-    if(args.length == 2){
-      port = Integer.parseInt(args[0]);
-      ip = args[0];
-    }
-    HttpServer server = HttpServer.create(new InetSocketAddress(ip,port), 0);
-    server.createContext("/welcome", new RequestHandler());
-    server.createContext("/headers", new RequestHeaders());
-    server.setExecutor(null); // Use the default executor
-    server.start();
-    System.out.println("Server started, listening on port: "+port);
-  }
+public class FirstHtttpServer
+{
 
-  static class RequestHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange he) throws IOException {
-      String response = "welcome to my very first almost home made Web Server :-)";
-      
-      StringBuilder sb = new StringBuilder();
-      sb.append("<!DOCTYPE html>\n");
-      sb.append("<html>\n");
-      sb.append("<head>\n");
-      sb.append("<title>My fancy Web Site</title>\n");
-      sb.append("<meta charset='UTF-8'>\n");
-      sb.append("</head>\n");
-      sb.append("<body>\n");
-      sb.append("<h2>welcome to my very first almost home made Web Server :-)</h2>\n");
-      sb.append("</body>\n");
-      sb.append("</html>\n");
-      response = sb.toString();
-      Headers h = he.getResponseHeaders();
-      h.add("Content-Type", "text/html");
-      he.sendResponseHeaders(200, response.length());
-      try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
-        pw.print(response); //What happens if we use a println instead of print --> Explain
-      }
-    }
-    
-  }
-  static class RequestHeaders implements HttpHandler {
-    @Override
-    public void handle (HttpExchange he) throws IOException{
-     String response = "welcome to my very first almost home made Web Server :-)";
-      StringBuilder sb = new StringBuilder();
-      String text = "";
-      Map headers = he.getRequestHeaders();
-      List keys;
-        for (int i = 0; i < headers.size(); i++) {
-            keys.add(i);
+    static int port = 8080;
+    static String ip = "127.0.0.1";
+
+    public static void main(String[] args) throws Exception
+    {
+        if (args.length == 2) {
+            port = Integer.parseInt(args[0]);
+            ip = args[0];
         }
-      
-//      Collection val = headers.values();
-//      Array key  = headers.keySet().;
-//      Object values [] = val.toArray();
-//        for (Object value : values) {
-//            text += value;
-//        }
-        System.out.println(text);
-     
-      sb.append("<!DOCTYPE html>\n");
-      sb.append("<html>\n");
-      sb.append("<head>\n");
-      sb.append("<title>My fancy Web Site</title>\n");
-      sb.append("<meta charset='UTF-8'>\n");
-      sb.append("</head>\n");
-      sb.append("<body>\n");
-      
-      sb.append("HEJ");
-      sb.append("</body>\n");
-      sb.append("</html>\n");
-      response = sb.toString();
-      
-      Headers h = he.getResponseHeaders();
-      h.add("Content-Type", "text/html");
-      he.sendResponseHeaders(200, response.length());
-      try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
-        pw.print(response); //What happens if we use a println instead of print --> Explain
-      }
-     
+        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
+        server.createContext("/welcome", new RequestHandler());
+        server.createContext("/headers", new RequestHeaders());
+        server.setExecutor(null); // Use the default executor
+        server.start();
+        System.out.println("Server started, listening on port: " + port);
     }
-  }
+
+    static class RequestHandler implements HttpHandler
+    {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException
+        {
+            String response = "welcome to my very first almost home made Web Server :-)";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<h2>welcome to my very first almost home made Web Server :-)</h2>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+        }
+
+    }
+
+    static class RequestHeaders implements HttpHandler
+    {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException
+        {
+            Headers headers = he.getRequestHeaders();
+            String response = "";
+            StringBuilder sb = new StringBuilder();
+            
+
+            sb.append("<!DOCTYPE html>");
+            sb.append("<html>");
+            sb.append("<head>");
+            sb.append("<title>My fancy Web Site</title>");
+            sb.append("<meta charset='UTF-8'>");
+            sb.append("</head>");
+            sb.append("<body>");
+            sb.append("<table border='1'>");
+            sb.append("<tr>");
+            sb.append("<th>Header</th><th>Values</th>");
+            sb.append("</tr>");
+            for(Map.Entry<String,List<String>> entry : headers.entrySet()){
+                sb.append("<tr>");
+                sb.append("<td>");
+                sb.append(entry.getKey());
+                sb.append("</td>");
+                sb.append("<td>");
+                for (String value : entry.getValue()){
+                    sb.append(value);
+                    sb.append("<br>");
+                }
+                sb.append("</td>");
+                sb.append("</tr>");
+            }
+            sb.append("</tr>");
+            sb.append("</tr>");
+            sb.append("</table>");
+            sb.append("</body>");
+            sb.append("</html>");
+            response = sb.toString();
+
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+
+        }
+    }
 }
